fc8522b6b74c5437f5db7b3835af8b5ad2b52b3f
pawelk <mon@op.pl>
2019-01-06 13:52:14 +0100

    automatic confirmed booking

--- inc/plugin-compatibility/plugin-compatibility.php
+++ inc/plugin-compatibility/plugin-compatibility.php
@@ -9,3 +9,5 @@ require get_template_directory() . '/inc/plugin-compatibility/jetpack/jetpack.ph
  * Load Woocommerce compatibility file.
  */
 require get_template_directory() . '/inc/plugin-compatibility/woocommerce/woocommerce.php';
+
+require get_template_directory() . '/inc/plugin-compatibility/restaurant-reservations/restaurant-reservations.php';
